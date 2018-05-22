83bc1340cbf50a84e6c3509eb58fca3a3d5e9a3c
Rus Hughes <russell.hughes@gmail.com>
2018-05-22 10:37:52 +0700

    completed all (?) tests

--- test/Vote.test.js
+++ test/Vote.test.js
@@ -5,6 +5,34 @@ const web3 = new Web3(ganache.provider());
 
 const compiledVote = require('../ethereum/build/Vote.json');
 
+const timeTravel = function (time) {
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.sendAsync({
+      jsonrpc: "2.0",
+      method: "evm_increaseTime",
+      params: [time], // 86400 is num seconds in day
+      id: new Date().getTime()
+    }, (err, result) => {
+      if(err){ return reject(err) }
+      return resolve(result)
+    });
+  })
+}
+
+const mineBlock = function (time) {
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.sendAsync({
+      jsonrpc: "2.0",
+      method: "evm_mine",
+      params: [],
+      id: new Date().getTime()
+    }, (err, result) => {
+      if(err){ return reject(err) }
+      return resolve(result)
+    });
+  })
+}
+
 beforeEach (async () => {
   accounts = await web3.eth.getAccounts();
 
@@ -18,4 +46,185 @@ describe('Vote', () => {
   it('deploys a vote contract', () => {
     assert.ok(vote.options.address);
   });
+
+  it('marks caller as the contract manager', async () => {
+    const manager = await vote.methods.manager().call();
+    assert.equal(accounts[0], manager);
+  });
+
+  it('contributors can be registered', async () => {
+    await vote.methods.registerContributor(accounts[0], '100').send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+    const request = await vote.methods.ballots(0).call();
+    assert.equal(accounts[0], request.contributor);
+    assert.equal('100', request.land);
+  });
+
+  it('only the manager can start the vote', async () => {
+    try {
+      await vote.methods.startVote().send({
+        from: accounts[1],
+        gas: '1000000'
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+    await vote.methods.startVote().send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+  });
+
+  it('contributors can not be registered if the contract is started', async () => {
+    await vote.methods.startVote().send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+    try {
+      await vote.methods.registerContributor(accounts[0], '100').send({
+        from: accounts[0],
+        gas: '1000000'
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('contributors can not vote if the contract has not been started', async () => {
+    await vote.methods.registerContributor(accounts[0], '100').send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+    try {
+      await vote.methods.registerVote('true').send({
+        from: accounts[0],
+        gas: '1000000'
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('contributors can vote when the contract is started', async () => {
+    await vote.methods.registerContributor(accounts[0], '100').send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    await vote.methods.startVote().send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    await vote.methods.registerVote('true').send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    const request = await vote.methods.ballots(0).call();
+    assert.equal(accounts[0], request.contributor);
+    assert.equal('100', request.land);
+
+    const summary = await vote.methods.getSummary().call();
+    assert.equal(true, summary[0]); //started
+    assert.equal(false, summary[2]); // ended
+    assert.equal(1, summary[3]); // voterCount
+    assert.equal(100, summary[4]); // landCount
+    assert.equal(1, summary[5]); // forVoteCount
+    assert.equal(0, summary[6]); // againstVoteCount
+    assert.equal(100, summary[7]); // forLandCount
+    assert.equal(0, summary[8]); // againstLandCount
+    assert.equal(1, summary[9]); // totalVoters
+    assert.equal(100, summary[10]); // totalLand
+  });
+
+  it('only registered contributors can vote', async () => {
+    await vote.methods.startVote().send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    try {
+      await vote.methods.registerVote('true').send({
+        from: accounts[0],
+        gas: '1000000'
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('after 14 days you can not vote', async () => {
+    await vote.methods.registerContributor(accounts[0], '100').send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    await vote.methods.startVote().send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    let timeBump = 1209600+1;
+    // timeBump = 100;
+
+    await timeTravel(timeBump);
+    await mineBlock();
+
+    try {
+      await vote.methods.registerVote('true').send({
+        from: accounts[0],
+        gas: '1000000'
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+
+    const summary = await vote.methods.getSummary().call();
+    assert.equal(true, summary[0]); //started
+    assert.equal(true, summary[2]); // ended
+    assert.equal(0, summary[3]); // voterCount
+    assert.equal(0, summary[4]); // landCount
+    assert.equal(0, summary[5]); // forVoteCount
+    assert.equal(0, summary[6]); // againstVoteCount
+    assert.equal(0, summary[7]); // forLandCount
+    assert.equal(0, summary[8]); // againstLandCount
+    assert.equal(1, summary[9]); // totalVoters
+    assert.equal(100, summary[10]); // totalLand
+  });
+
+  it('contributors can only vote once', async () => {
+    await vote.methods.registerContributor(accounts[0], '100').send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    await vote.methods.startVote().send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    await vote.methods.registerVote('true').send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    try {
+      await vote.methods.registerVote('true').send({
+        from: accounts[0],
+        gas: '1000000'
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
 });
