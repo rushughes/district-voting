ab8ffe65b0becc65126afc11fef94a91d235787f
Rus Hughes <russell.hughes@gmail.com>
2018-05-22 15:36:29 +0700

    added example config file

---
+++ config.example.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mnemonic: 'this is the mnemonic to your ethereum wallet for deploying the contract and populating data',
+  network: 'https://rinkeby.infura.io/networkidhere',
+  voteContractAddress: '0x1797e6391A50d8C7D35C766ceB7A9AE2ff4bBA44',
+  title: 'Virtual Sand Hill Road District Approval'
+};
